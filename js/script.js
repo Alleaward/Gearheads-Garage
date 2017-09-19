2fc99d505d867253ac9f0de75622a1e761dd42ff
Allea Ward <Allea_ward@hotmail.com>
2017-09-20 02:59:49 +1000

    Gearheads Garage 0.4
    - Carousel images now retain aspect ration.
    - Carousel images are now timed and clickable

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+var slideIndex = 0;
 
-var slideIndex = 1;
+//Button Changer
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -19,4 +20,21 @@ function showDivs(n) {
      x[i].style.display = "none";
   }
   x[slideIndex-1].style.display = "block";
+}
+
+//Automatic Changer
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1
+    }
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3500); // Change image every 2 seconds
 }
