b58760a2ac0198803df063432eb80bfe4157425b
Allea Ward <Allea_ward@hotmail.com>
2017-11-20 02:40:28 +1000

    Changing to CDNS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
-var slideIndex=0;showDivs(slideIndex);function plusDivs(n){showDivs(slideIndex+=n)}
-function showDivs(n){var i;var x=document.getElementsByClassName("mySlides");if(n>x.length){slideIndex=1}
-if(n<1){slideIndex=x.length}
-for(i=0;i<x.length;i++){x[i].style.display="none"}
-x[slideIndex-1].style.display="block"}
-showSlides();function showSlides(){var i;var slides=document.getElementsByClassName("mySlides");for(i=0;i<slides.length;i++){slides[i].style.display="none"}
-slideIndex++;if(slideIndex>slides.length){slideIndex=1}
-slides[slideIndex-1].style.display="block";setTimeout(showSlides,3000)}
+var slideIndex = 0; showDivs(slideIndex); 
+function plusDivs(n) { 
+    showDivs(slideIndex += n) 
+}
+function showDivs(n) {
+    var i; 
+    var x = document.getElementsByClassName("mySlides"); 
+    if (n > x.length) { 
+        slideIndex = 1 
+    }
+    if (n < 1) { 
+        slideIndex = x.length 
+    }
+    for (i = 0; i < x.length; i++) { 
+        x[i].style.display = "none" 
+    }
+    x[slideIndex - 1].style.display = "block"
+}
+showSlides(); function showSlides() {
+    var i; 
+    var slides = document.getElementsByClassName("mySlides"); 
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none" 
+    }
+    slideIndex++; 
+    if (slideIndex > slides.length) { 
+        slideIndex = 1 
+    }
+    slides[slideIndex - 1].style.display = "block"; 
+    setTimeout(showSlides, 3000);
+}
